2d9684f0200e0b010f0158436ccfa797080e88cf
griff <griffinmarshallr@outlook.com>
2026-02-15 20:31:46 +0100

    Railway config and changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import charactersRouter       from './routes/characters.js'
 import itemsRouter            from './routes/items.js'
 import buildsRouter           from './routes/builds.js'
 import leaderboardRouter      from './routes/leaderboard.js'
 import communityBuildsRouter  from './routes/community-builds.js'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const clientDist = path.resolve(__dirname, '../../client/dist')
 
 const app = express()
 
@@ -15,6 +20,12 @@ app.use('/api', itemsRouter)
 app.use('/api', buildsRouter)
 app.use('/api', leaderboardRouter)
 app.use('/api', communityBuildsRouter)
+
+// Serve the built React client (production)
+app.use(express.static(clientDist))
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(clientDist, 'index.html'))
+})
 
 // catch-all error handler – prevents stack traces reaching the client
 app.use((err, _req, res, _next) => {
